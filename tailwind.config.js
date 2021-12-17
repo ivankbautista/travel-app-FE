8eb09d7bb35b4f5196297e1bbe739220abfaaf41
bananakiu <leandrenoelkiu@gmail.com>
2021-12-17 18:34:38 +0800

    configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'atlas': {
+          700: '#202020',
+          600: '#3e0e62',
+          500: '#4b077c',
+          400: '#7a1abe',
+          300: '#b784db',
+          200: '#d5b9ea',
+          100: '#f3eef8',
+        },
+      },
+    },
   },
   plugins: [],
 }
