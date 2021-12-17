96cec0534c9132e8034a783daf3cad8787c70a27
Airene Maria <aireneapacible@gmail.com>
2021-12-17 20:14:46 +0800

    Fixes merge conflicts

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,18 +20,6 @@ export default function HomePage() {
       <p className='mt-10 text-3xl text-center text-rose-500 font-bold underline'>
         Hi team! Here's our Homepage!
       </p>
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-        <div className='flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full'>
-          {countries.map((e) => (
-            <div className='mb-3' key={e.c}>
-              <Link as={`countries/${e.c}`} href='/countries/[country]'>
-                <a>
-                  <div className='p-4 mt-4 text-center border w-52 rounded-xl hover:text-white hover:bg-gray-800'>
-                    <div>{e.c}</div>
-                    <div>{e.capital}</div>
-                    <div>Rating: {e.rating}</div>
-                  </div>
-                </a>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
         {countries.map(e =>
