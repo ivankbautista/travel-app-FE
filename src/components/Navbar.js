1ee97726580cfb54c49b874e52f0f2acef145a66
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-12-20 23:36:38 +0800

    merge with main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,23 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='flex bg-purple-700 justify-between items-center px-8 h-16'>
+    <div className='flex bg-purple-700 justify-between items-center px-10 h-14 bg-atlas-400'>
       <div>
         <a href='#' className='text-white'>
-          Logo
+          {/* <img src='../../public/plane.png' alt='Logo' /> */}
+          <img src='plane.png' alt='Logo' height='30' width='30' />
         </a>
+        {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
       </div>
-      <div className='flex w-60 justify-between text-white'>
+      <div className='flex w-60 justify-between'>
         <Link href='#'>
-          <a>Explore</a>
+          <a className='text-atlas-100 hover:text-atlas-700'>Explore</a>
         </Link>
         <Link href='#'>
-          <a>My Rolls</a>
+          <a className='text-atlas-100 hover:text-atlas-700'>My Rolls</a>
         </Link>
         <Link href='#'>
-          <a>My Profile</a>
+          <a className='text-atlas-100 hover:text-atlas-700'>My Profile</a>
         </Link>
       </div>
     </div>
