519183256c08e6c2fa326f0c82d63ef40b9a0eed
bananakiu <leandrenoelkiu@gmail.com>
2022-01-02 17:32:53 +0800

    refactor code to remove formfield (can't work as separate component if using react hook forms

--- src/pages/users/sign_in.js
+++ src/pages/users/sign_in.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { FormContainer } from '../../components/shared/FormContainer'
 import { FormFieldLabel } from '../../components/shared/FormFieldLabel'
-import { FormField } from '../../components/shared/FormField'
+import { useForm } from "react-hook-form";
 
 export const sign_in = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (<>
         <div class="
         flex flex-col justify-center items-center
@@ -11,48 +14,62 @@ export const sign_in = () => {
         py-6 px-8
         " style={{height: "calc(100vh - 3.5rem)"}}>
             <FormContainer>
-                <h2 class="text-2xl font-semibold mb-4">
-                    Login
-                </h2>
-                <div className="space-y-2 mb-4">
-                    <FormFieldLabel>
-                        Email Address
-                    </FormFieldLabel>
-                    <FormField
-                        type="email"
-                        placeholder="johndoe@gmail.com"
-                    />
-                </div>
-                <div className="space-y-2 mb-4">
-                    <FormFieldLabel>
-                        Password
-                    </FormFieldLabel>
-                    <FormField
-                        type="password"
-                        placeholder="***********"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    class="
-                    flex justify-center
-                    mt-4 p-3 w-full 
-                    bg-atlas-400 hover:bg-atlas-500 
-                    text-gray-100 font-semibold
-                    rounded-lg shadow-lg
-                    cursor-pointer transition ease-in duration-100
-                    "
-                >
-                    Log In
-                </button>
-                <div class="flex flex-col justify-center items-center mt-3">
-                    <a href="/users/sign_up"className="
-                    text-atlas-400 hover:text-atlas-500
-                    text-sm font-semibold
-                    ">
-                        Don't have an account?
-                    </a>
-                </div>
+                <form onSubmit = { handleSubmit(onSubmit) }>
+                    <h2 class="text-2xl font-semibold mb-4">
+                        Login
+                    </h2>
+                    <div className="space-y-2 mb-4">
+                        <FormFieldLabel>
+                            Email Address
+                        </FormFieldLabel>
+                        <input
+                            type="email"
+                            {...register("email")}
+                            placeholder={ "johndoe@gmail.com" }
+                            className="
+                            w-full text-base px-4 py-2 border
+                            border-gray-300 rounded-lg
+                            focus:outline-none focus:border-atlas-400
+                            "
+                        ></input>
+                    </div>
+                    <div className="space-y-2 mb-4">
+                        <FormFieldLabel>
+                            Password
+                        </FormFieldLabel>
+                        <input
+                            type="password"
+                            {...register("password")}
+                            placeholder={ "***********" }
+                            className="
+                            w-full text-base px-4 py-2 border
+                            border-gray-300 rounded-lg
+                            focus:outline-none focus:border-atlas-400
+                            "
+                        ></input>
+                    </div>
+                    <button
+                        type="submit"
+                        class="
+                        flex justify-center
+                        mt-8 p-3 w-full 
+                        bg-atlas-400 hover:bg-atlas-500 
+                        text-gray-100 font-semibold
+                        rounded-lg shadow-lg
+                        cursor-pointer transition ease-in duration-100
+                        "
+                    >
+                        Log In
+                    </button>
+                    <div class="flex flex-col justify-center items-center mt-3">
+                        <a href="/users/sign_up"className="
+                        text-atlas-400 hover:text-atlas-500
+                        text-sm font-semibold
+                        ">
+                            Don't have an account?
+                        </a>
+                    </div>
+                </form>
             </FormContainer>
         </div>
     </>)
--- src/pages/users/sign_up.js
+++ src/pages/users/sign_up.js
@@ -2,62 +2,11 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { FormContainer } from '../../components/shared/FormContainer';
-import { FormField } from '../../components/shared/FormField';
 import { FormFieldLabel } from '../../components/shared/FormFieldLabel';
 
 export const sign_up = () => {
-  // constants
-  // TODO: MOVE TO AN IMPORT
-  const BASE_URL = "http://localhost:1234"
-
-  // states
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    let errorList = [];
-
-    // create user obj
-    let createdUser = { user: {
-        email: data.email,
-        password: data.password,
-      }
-    };
-  
-    // handle errors
-    // errorList = handleErrors(createdUser, errorList);
-
-    // POST to server
-    axios({
-      method: 'POST',
-      url: `${BASE_URL}/users/`,
-      data: createdUser,
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }).then((response) => {
-      // set API bearer token (user's token)
-      console.log(createdUser)
-      console.log("success")
-      console.log(response.headers)
-      // console.log(response.headers['Authorization'])
-    }).catch((error) => {
-      console.log(createdUser)
-      console.log("fail")
-      console.log(error.response)
-      // console.log(error?.response?.data?.errors?.full_messages) // ! TEMP
-      // errorList.push(...error?.response?.data?.errors?.full_messages);
-      // setErrors(errorList);
-    })
-    // .then(() => {
-    //     if (errorList.length === 0) {
-    //         // show relevant pages
-    //         openPage("dashboard");
-
-    //         // ! TEMP: alert (turn into nicer alerts)
-    //         alert('User successfully created! You are logged in!');
-    //     }
-    // });
-  };
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data) => {};
 
   return (<>
     <div class="
@@ -67,84 +16,126 @@ export const sign_up = () => {
     overflow-auto
     " style={{height: "calc(100vh - 3.5rem)"}}>
       <FormContainer>
-        <h2 class="text-2xl font-semibold mb-4">
-          Sign Up
-        </h2>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
+        <form onSubmit = { handleSubmit(onSubmit) }>
+          <h2 class="text-2xl font-semibold mb-4">
+            Sign Up
+          </h2>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
               Email Address
-          </FormFieldLabel>
-          <FormField
+            </FormFieldLabel>
+            <input
               type="email"
-              placeholder="johndoe@gmail.com"
-          />
-        </div>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
+              {...register("email")}
+              placeholder={ "johndoe@gmail.com" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+          </div>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
               Username
-          </FormFieldLabel>
-          <FormField
+            </FormFieldLabel>
+            <input
+              type="text"
+              {...register("username")}
+              placeholder={ "johndoe" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+            {/* <FormField
               type="text"
               placeholder="johndoe"
-          />
-        </div>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
+            /> */}
+          </div>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
               First Name
-          </FormFieldLabel>
-          <FormField
+            </FormFieldLabel>
+            <input
               type="text"
-              placeholder="Jon"
-          />
-        </div>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
-              Last Name
-          </FormFieldLabel>
-          <FormField
+              {...register("first_name")}
+              placeholder={ "Jonathan" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+          </div>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
+                Last Name
+            </FormFieldLabel>
+            <input
               type="text"
-              placeholder="Doe"
-          />
-        </div>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
+              {...register("last_name")}
+              placeholder={ "Donatello" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+          </div>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
               Password
-          </FormFieldLabel>
-          <FormField
+            </FormFieldLabel>
+            <input
               type="password"
-              placeholder="***********"
-          />
-        </div>
-        <div className="space-y-2 mb-4">
-          <FormFieldLabel>
-              Confirm Password
-          </FormFieldLabel>
-          <FormField
+              {...register("password")}
+              placeholder={ "***********" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+          </div>
+          <div className="space-y-2 mb-4">
+            <FormFieldLabel>
+                Confirm Password
+            </FormFieldLabel>
+            <input
               type="password"
-              placeholder="***********"
-          />
-        </div>
-        <button
-          type="submit"
-          class="
-          flex justify-center
-          mt-4 p-3 w-full 
-          bg-atlas-400 hover:bg-atlas-500 
-          text-gray-100 font-semibold
-          rounded-lg shadow-lg
-          cursor-pointer transition ease-in duration-100
-          "
-        >
-          Sign Up
-        </button>
-        <div class="flex flex-col justify-center items-center mt-3">
-          <a href="/users/sign_in"className="
-          text-atlas-400 hover:text-atlas-500
-          text-sm font-semibold
-          ">
-            Already have an account?
-          </a>
-        </div>
+              {...register("confirm_password")}
+              placeholder={ "***********" }
+              className="
+              w-full text-base px-4 py-2 border
+              border-gray-300 rounded-lg
+              focus:outline-none focus:border-atlas-400
+              "
+            ></input>
+          </div>
+          <button
+            type="submit"
+            class="
+            flex justify-center
+            mt-4 p-3 w-full 
+            bg-atlas-400 hover:bg-atlas-500 
+            text-gray-100 font-semibold
+            rounded-lg shadow-lg
+            cursor-pointer transition ease-in duration-100
+            "
+          >
+            Sign Up
+          </button>
+          <div class="flex flex-col justify-center items-center mt-3">
+            <a href="/users/sign_in"className="
+            text-atlas-400 hover:text-atlas-500
+            text-sm font-semibold
+            ">
+              Already have an account?
+            </a>
+          </div>
+        </form>
       </FormContainer>
     </div>
   </>)
