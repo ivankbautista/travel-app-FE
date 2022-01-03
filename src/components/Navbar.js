9d91381d00b1d1967c6bb69c5f71af25610b31a5
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-03 20:30:23 +0800

    Fix image logo rendering on all pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,14 @@
+import Image from 'next/image'
+import logo from '../../public/logo.png'
 import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-8 h-14 bg-atlas-400'>
-      <div>
-        <a href='/' className='text-white'>
-          {/* <img src='../../public/plane.png' alt='Logo' /> */}
-          <img
-            src='logo.png'
-            alt='Logo'
-            height='30'
-            width='30'
-            className='text-white'
-          />
-        </a>
-        {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
-      </div>
+      <a href='/' className='flex justify-center items-center text-white h-full'>
+        <Image src={logo} height={'30'} width={'30'} w className='object-contain m-0 h-full'/>
+      </a>
+      {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
       <div className='flex w-60 justify-between'>
         <Link href='#'>
           <a className='text-atlas-100 hover:text-atlas-200'>Explore</a>
