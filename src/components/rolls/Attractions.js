68f5baa4070e6a3d6ff5f2f9ca318f40398eea3e
Airene Maria <aireneapacible@gmail.com>
2022-01-03 18:06:01 +0800

    Updates pictures for attractions

--- src/components/rolls/Attractions.js
+++ src/components/rolls/Attractions.js
@@ -7,22 +7,22 @@ export const Attractions = () => {
                 <div className="grow flex items-center justify-center w-5/6">
                 <div className="flex flex-col text-xs">
                 <div className="flex items-center justify-center flex-row">
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.touropia.com/gfx/d/famous-temples/golden_pavilion.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.planetware.com/wpimages/2020/09/japan-top-attractions-osaka-castle.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://img.traveltriangle.com/blog/wp-content/uploads/2018/02/places-to-visit-in-Japan1.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.touropia.com/gfx/d/largest-castles-in-the-world/himeji_castle.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
                 </div>
                 <div className="flex items-center justify-center flex-row">
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.neverendingvoyage.com/wp-content/uploads/2017/11/planning-a-trip-to-japan-1100x733.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://japantoday-asset.scdn3.secure.raxcdn.com/img/store/40/50/70a678243caeee7cd54e67d25c6656c27b7d/itsukushima.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://3m4r5618el913vtfz3jffby9-wpengine.netdna-ssl.com/wp-content/uploads/things-to-do-in-japan-4-1.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.bedroomchecker.com/wp-content/uploads/2013/10/Tokyo-Japan.png')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
                 </div>
                 <div className="flex items-center justify-center flex-row">
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
-                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.topfivebuzz.com/wp-content/uploads/2017/08/Itsukushima-Shrine.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.planetware.com/photos-large/JPN/japan-attractions-imperial-palace.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://blog.japanwondertravel.com/wp-content/uploads/2021/02/kevin-xie-vW_gySwaQ9k-unsplash-scaled.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://www.everysteph.com/wp-content/uploads/2020/02/meiji-jingu-tokyo-historical-places-of-Japan-1-1024x683.jpg')]"><span className="bg-atlas-300 p-2 rounded-sm">Place A</span></div>
                 </div>
                 <div className="font-light font-display text-2xl text-center mt-5">Enjoy sightseeing?</div>
                 </div>
