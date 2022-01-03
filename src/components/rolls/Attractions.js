98904a7ec40f47f009fb670573bcdac975adb22b
Airene Maria <aireneapacible@gmail.com>
2022-01-03 18:02:50 +0800

    Styles attractions

--- src/components/rolls/Attractions.js
+++ src/components/rolls/Attractions.js
@@ -1,13 +1,31 @@
 import { RollBorder } from './RollBorder';
-
-
 export const Attractions = () => {
     return (
         <div>
-            <div className="h-[92vh] bg-gradient-to-b from-[#26083d] to-atlas-700 flex flex-wrap snap-center">
+            <div className="h-[92vh] bg-gradient-to-b from-[#381254] to-atlas-600 flex flex-wrap snap-center">
                 <RollBorder />
-                <div className="grow">
-                    attractions
+                <div className="grow flex items-center justify-center w-5/6">
+                <div className="flex flex-col text-xs">
+                <div className="flex items-center justify-center flex-row">
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                </div>
+                <div className="flex items-center justify-center flex-row">
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                </div>
+                <div className="flex items-center justify-center flex-row">
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/564x/da/9e/30/da9e30a05260b461918e16c83f748931.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/736x/0c/56/4c/0c564caa6daa9e801a63af5cf7f52301.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://p.favim.com/orig/2018/10/09/aesthetic-aesthetic-food-japanese-Favim.com-6432958.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                    <div className="rounded-md w-60 h-40 p-2 pt-3 text-right m-0.5 bg-cover bg-[url('https://i.pinimg.com/originals/96/5b/70/965b70cc9f9dfcd22736b584bcf537cd.jpg')]"><span className="bg-atlas-200 p-2 rounded-sm">Place A</span></div>
+                </div>
+                <div className="font-light font-display text-2xl text-center mt-5">Enjoy sightseeing?</div>
+                </div>
                 </div>
                 <RollBorder />
             </div>
--- src/components/rolls/Intro.js
+++ src/components/rolls/Intro.js
@@ -7,7 +7,7 @@ export const Intro = () => {
                 <div className="grow flex items-center justify-center w-3/4">
                     <div className="font-display bg-[url('https://c4.wallpaperflare.com/wallpaper/568/75/606/cherry-blossom-volcano-mountain-fuji-wallpaper-preview.jpg')] bg-clip-text text-transparent text-center">
                         <div className="font-extrabold text-9xl">TOKYO<br/>JAPAN</div>
-                        <div className="font-thin text-white text-3xl">January 3 to 6, 2022</div>
+                        <div className="font-thin text-white text-3xl">January 3 to 7, 2022</div>
                     </div>
                 </div>
                 <RollBorder />
