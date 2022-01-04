5515ca57c8d0718a24652cc398f08bc627e464b6
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-04 19:53:46 +0800

    Small fix to the header

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -7,15 +7,28 @@ const countries = [
 
 export const UserProfile = () => {
     return (
-        <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
+      <>
+        <div className="border-2 h-screen">
+          <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
             <Image src={logo} height={'120'} width={'120'} className='bg-red-400 rounded-full mt-3'/>
             {/* <p className="rounded-full w-32 h-32 bg-red-400 mt-3"></p> */}
             <h1 className="text-white text-7xl">Leandre Kiu</h1>
             <div className="flex justify-between w-56 h-1/4">
-                <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
-                <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
+              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
+              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
             </div>
+          </div>
+
+          <div className="flex items-center ">
+            <p className="h-96 w-3/12 text-white border-2 flex flex-col justify-center items-center">bio/user stats</p>
+
+            <div className="w-9/12 border-2">
+              <p className="text-white">Hello</p>
+            </div>
+
+          </div>
         </div>
+      </>
     )
 }
 
