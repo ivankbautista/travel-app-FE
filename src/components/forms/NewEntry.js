f7d1ced029112f940e27270d8be532d49ae87e93
Airene Maria <aireneapacible@gmail.com>
2022-01-04 17:59:06 +0800

    Creates files for forms

---
+++ src/components/forms/NewEntry.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import FormContainer from '../shared/FormContainer'
+import FormField from '../shared/FormField'
+import FormFieldLabel from '../shared/FormFieldLabel'
--- src/components/shared/FormField.js
+++ src/components/shared/FormField.js
@@ -1,16 +1,16 @@
-// import React from 'react'
+import React from 'react'
 
-// export const FormField = ({ fieldName, type, placeholder, register }) => {
-//     return (
-//         <input
-//             type={ type }
-//             {...register({ fieldName })}
-//             placeholder={ placeholder }
-//             className="
-//             w-full text-base px-4 py-2 border
-//             border-gray-300 rounded-lg
-//             focus:outline-none focus:border-atlas-400
-//             "
-//         ></input>
-//     )
-// }
+export const FormField = ({ fieldName, type, placeholder, register }) => {
+    return (
+        <input
+            type={ type }
+            {...register({ fieldName })}
+            placeholder={ placeholder }
+            className="
+            w-full text-base px-4 py-2 border
+            border-gray-300 rounded-lg
+            focus:outline-none focus:border-atlas-400
+            "
+        ></input>
+    )
+}
