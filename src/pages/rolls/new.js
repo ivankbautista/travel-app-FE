b0fd20e38eadf12a29cf42b652a99bc651ab4bb2
Airene Maria <aireneapacible@gmail.com>
2022-01-04 18:35:29 +0800

    Tests out some fields

---
+++ src/pages/rolls/new.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import axios from 'axios';
+import FormContainer from '../../components/shared/FormContainer'
+import FormFieldLabel from '../../components/shared/FormFieldLabel'
+import { useForm } from "react-hook-form";
+
+const NewRoll = (props) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data) => {}
+    return (
+    <div>
+        <div class="
+        flex flex-col justify-center items-center
+        w-full bg-atlas-700
+        py-6 px-8
+        " style={{height: "calc(100vh - 3.5rem)"}}>
+            <FormContainer>
+                <form onSubmit = { handleSubmit(onSubmit) }>
+                    <h2 class="text-2xl font-semibold mb-4">
+                        New Roll
+                    </h2>
+                    <FormFieldLabel>
+                        Title
+                    </FormFieldLabel>
+                    <input
+                        type="string"
+                        {...register("title")}
+                        placeholder={ "Japan 2022" }
+                        className="
+                        w-full text-base px-4 py-2 border
+                        border-gray-300 rounded-lg
+                        focus:outline-none focus:border-atlas-400
+                        "
+                        required
+                    >
+                    </input>
+                </form>
+            </FormContainer>
+        </div>
+    </div>
+  )
+}
+
+export default NewRoll
