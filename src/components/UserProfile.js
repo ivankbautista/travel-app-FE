64cf89d8d642c5825d94051d7745c8dfaf441836
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-07 19:46:30 +0800

    Display my entries page or my rolles page depending on click of button

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,75 +1,51 @@
 import Image from 'next/image'
 import logo from '../../public/logo.png'
-
-const countries = [
-    {c: 'Japan', rating: '4.5', imageUrl: "https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg"}
-  ]
+import MyEntries from './shared/MyEntries'
+import MyRolls from './shared/MyRolls'
+import { useState } from 'react'
 
 export const UserProfile = () => {
-    return (
-      <>
-        {/* Profile header  */}
-        <div className="h-screen">
-          <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
-            <Image src={logo} height={'120'} width={'120'} className='bg-red-400 rounded-full mt-3'/>
-            {/* <p className="rounded-full w-32 h-32 bg-red-400 mt-3"></p> */}
-            <h1 className="text-white text-7xl">Leandre Kiu</h1>
-            <div className="flex justify-between w-56 h-1/4">
-              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
-              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
-            </div>
-          </div>
-          
-          {/* Sidebar & Main content */}
-          <div className="flex items-center">
-            <p style={{height: "68vh"}} className="w-3/12 text-white flex flex-col justify-center items-center">bio/user stats</p>
+  
+  const [isMyEntriesPage, setIsMyEntriesPage] = useState(true);
+  const [isMyRollsPage, setIsMyRollsPage] = useState(false);
 
-            <div style={{height: "68vh"}} className="w-9/12 bg-gray-900">
-              <button className="bg-purple-400 px-8 py-3 mt-3 ml-3">My entries</button>
-              <button className="bg-gray-300 px-8 py-3 mt-3">My Rolls</button>
+  const displayPage = (page) => {
+    setIsMyEntriesPage(page === "myEntries" ? true : false)
+    setIsMyRollsPage(page === "myRolls" ? true : false)
+  }
 
-              <div className="ml-3 mt-4 pr-3">
-                <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-                  <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-                  <div className="w-full pl-4 py-2">
-                    <p className="text-white">Kyoto, Japan</p>
-                    <p className="text-white">12/17/2021</p>
-                  </div>
-                  <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-                </div>
-
-                <div className="bg-white h-48 p-3 ">
-                  <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-                </div>
-
-              </div>
-              
-              <div className="ml-3 mt-4 pr-3">
-                <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-                  <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-                  <div className="w-full pl-4 py-2">
-                    <p className="text-white">Tokyo, Japan</p>
-                    <p className="text-white">12/19/2021</p>
-                  </div>
-                  <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-                </div>
-
-                <div className="bg-white h-48 p-3 ">
-                  <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-                </div>
-
-              </div>
-
-            </div>
-
+  return (
+    <>
+      {/* Profile header  */}
+      <div className="h-screen">
+        <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
+          <Image src={logo} height={'120'} width={'120'} className='bg-red-400 rounded-full mt-3'/>
+          {/* <p className="rounded-full w-32 h-32 bg-red-400 mt-3"></p> */}
+          <h1 className="text-white text-7xl">Leandre Kiu</h1>
+          <div className="flex justify-between w-56 h-1/4">
+            <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
+            <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
           </div>
         </div>
-      </>
-    )
+        
+        {/* Sidebar & Main content */}
+        <div className="flex items-center">
+          <p style={{height: "68vh"}} className="w-3/12 text-white flex flex-col justify-center items-center">bio/user stats</p>
+
+          <div className="h-full w-9/12 bg-gray-900">
+            <button className={`${isMyEntriesPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3 ml-3`} onClick={() => displayPage("myEntries")}> My entries </button>
+            <button className={`${isMyRollsPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3`} onClick={() => displayPage("myRolls")}>My Rolls</button>
+
+
+            {isMyEntriesPage && <MyEntries/>}
+            {isMyRollsPage && <MyRolls/>}
+
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
 // function App() {
---
+++ src/components/shared/MyRolls.js
@@ -0,0 +1,13 @@
+function MyRolls() {
+  return (
+    <div>
+      <div className="h-screen ml-3 mt-4 pr-3 overflow-auto">
+        <p className="text-white">My Rolls Page</p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default MyRolls
