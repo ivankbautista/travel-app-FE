43cf289f24744f51f5cc693e9203f7d4e03087e5
bananakiu <leandrenoelkiu@gmail.com>
2022-01-08 11:18:33 +0800

    setup login user context states

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import logo from '../../public/logo.png'
 import Link from 'next/link'
+import { useContext } from "react"; // import Context hook
+import HeaderContext from "../contexts/HeaderContext"; // import the context you want to draw the variable from
 
 const Navbar = () => {
-  let logged_in = true; // TEMP
+  const {
+    loggedIn,
+    setLoggedIn,
+  } = useContext(HeaderContext) // destructing to get menuItems from HeaderContext
 
   return (
     <div className='flex justify-between items-center px-8 h-14 bg-atlas-400'>
@@ -26,7 +31,7 @@ const Navbar = () => {
             My Rolls
           </a>
         </Link>
-        { logged_in ?
+        { loggedIn ?
           <>
             <Link href='#'>
               <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
