ec6766cee82f11891065c1185dcf02cdd10950d5
bananakiu <leandrenoelkiu@gmail.com>
2022-01-08 11:05:25 +0800

    made navbar dynamic based on login status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,49 @@ import logo from '../../public/logo.png'
 import Link from 'next/link'
 
 const Navbar = () => {
+  let logged_in = false; // TEMP
+
   return (
     <div className='flex justify-between items-center px-8 h-14 bg-atlas-400'>
-      <a href='/' className='flex justify-center items-center text-white h-full'>
-        <Image src={logo} height={'30'} width={'30'} w className='object-contain m-0 h-full'/>
-      </a>
-      {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
-      <div className='flex w-60 justify-between'>
+      {/* logo */}
+      <div className='h-full'>
+        <a href='/' className='flex justify-center items-center text-white h-full'>
+          {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+          <Image src={logo} height={'30'} width={'30'} w className='object-contain m-0 h-full'/>
+        </a>
+      </div>
+      {/* nav-items-left */}
+      <div className='flex justify-between items-center h-full'>
         <Link href='#'>
-          <a className='text-atlas-100 hover:text-atlas-200'>Explore</a>
+          <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
+            Explore
+          </a>
         </Link>
         <Link href='#'>
-          <a className='text-atlas-100 hover:text-atlas-200'>My Rolls</a>
+          <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
+            My Rolls
+          </a>
         </Link>
-        <Link href='#'>
-          <a className='text-atlas-100 hover:text-atlas-200'>My Profile</a>
-        </Link>
+        { logged_in ?
+          <Link href='#'>
+            <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
+              My Profile
+            </a>
+          </Link>
+        :
+          <>
+            <Link href='/users/sign_in'>
+              <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
+                Log in
+              </a>
+            </Link>
+            <Link href='/users/sign_up'>
+              <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>
+                Sign Up
+              </a>
+            </Link>
+          </>
+        }
       </div>
     </div>
   )
