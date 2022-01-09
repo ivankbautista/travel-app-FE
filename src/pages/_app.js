5154c95046cc5eb29264e0de3a866f81ef6c032d
bananakiu <leandrenoelkiu@gmail.com>
2022-01-09 23:10:23 +0800

    load data of all users upon initialization of app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,27 @@ import Navbar from '../components/Navbar'
 import '/styles/globals.css'
 import ContextWrapper from '../components/ContextWrapper'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function MyApp({ Component, pageProps }) {
-  const [initialized, setInitialized] = useState(false)
+  const API = "http://localhost:3001"
+  const [initialized, setInitialized] = useState(false) // setup for localStorage
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
+    // setup for localStorage
     setInitialized(true)
+
+    // load in user data
+    axios({
+      method: 'GET',
+      url: `${API}/users/`,
+    }).then((response) => {
+      // console.log(response.data); //TEMP
+      setUsers(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
   }, [])
 
   return (
