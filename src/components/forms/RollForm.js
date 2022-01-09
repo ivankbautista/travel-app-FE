be0691d978c2c52b970de0ff88ef82a5dd860dcb
Airene Maria <aireneapacible@gmail.com>
2022-01-09 14:37:55 +0800

    Creates submit button

--- src/components/forms/RollForm.js
+++ src/components/forms/RollForm.js
@@ -8,7 +8,7 @@ export const RollForm = (props) => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = (data) => {}
     return (
-    <div>
+    <>
         <div class="
         flex flex-col justify-center items-center
         w-full bg-atlas-700
@@ -77,10 +77,23 @@ export const RollForm = (props) => {
                         required
                     >
                     </input>
+                    <button
+                        type="submit"
+                        class="
+                        flex justify-center
+                        mt-8 p-3 w-full 
+                        bg-atlas-400 hover:bg-atlas-500 
+                        text-gray-100 font-semibold
+                        rounded-lg shadow-lg
+                        cursor-pointer transition ease-in duration-100
+                        "
+                    >
+                        Create
+                    </button>
                 </form>
             </FormContainer>
         </div>
-    </div>
+    </>
   )
 }
 
