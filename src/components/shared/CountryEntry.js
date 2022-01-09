912d029f8e282bbd6a56e0fdb49e91d631cbf153
ivankbautista <ivankevinbautista@gmail.com>
2022-01-09 17:09:13 +0800

    Add in tentative my entries page

---
+++ src/components/shared/CountryEntry.js
@@ -0,0 +1,11 @@
+function CountryEntry() {
+    return (
+        <div>
+            <div className="h-screen ml-3 mt-4 pr-3 overflow-auto">
+                <p className="text-white">My Entries Page</p>
+            </div>
+        </div>
+    )
+}
+
+export default CountryEntry
