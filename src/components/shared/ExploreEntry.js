c5ed9b650579ee23b166bad0f9606f78f64a3755
ivankbautista <ivankevinbautista@gmail.com>
2022-01-10 00:34:10 +0800

    Change atlas color to match explore entry color pattern

--- src/components/shared/ExploreEntry.js
+++ src/components/shared/ExploreEntry.js
@@ -1,16 +1,14 @@
 import logo from '../../../public/logo.png'
 import Image from 'next/image'
 
-
 function ExploreEntry({ exploreEntry }) {
     return (
         <div>
             <div className="mt-6">
-                <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-                    <div className="text-white font-bold text-2xl items-center w-10 h-10 rounded-full">{exploreEntry.rating}</div>
+                <div className="flex justify-between items-center py-3 px-3 bg-atlas-400">
+                    <div className="text-white font-bold text-2xl items-center w-10 h-10 border rounded-full">{exploreEntry.rating}</div>
                     <div className="w-full pl-4 py-2">
-                        <p className="text-white">{exploreEntry.username}</p>
+                        <p className="text-white font-bold">{exploreEntry.username}</p>
                         <p className="text-white">{exploreEntry.date}</p>
                     </div>
                     <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full' />
