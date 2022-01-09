db74d51d2efa91673f5d915c979938e1bb0b9fbd
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-09 12:39:57 +0800

    Make Entry component for each data and add dummy data

---
+++ entryData.js
@@ -0,0 +1,42 @@
+export const entriesData = [
+  {
+    id: '1',
+    countryName: 'Japan',
+    date: '9/12/2021',
+    flagIcon: 'jp',
+    activityIcon: '🇯🇵',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At earum alias a facere aperiam ipsum officia quaerat voluptas nisi. Molestiae ullam praesentium nisi. Tempora neque ea commodi numquam. Placeat ipsum sint esse! Alias mollitia similique sint adipisci aperiam blanditiis repellat ut quia, quisquam vitae perspiciatis nesciunt eius sed rerum harum cumque voluptatem dolore. Laudantium commodi autem voluptatem recusandae eligendi fugiat, officiis ipsa? Ut harum dignissimos impedit asperiores ex voluptatum optio vitae eligendi enim? Quam earum nisi voluptatem dolores illo mollitia beatae debitis. Error eligendi at quos cupiditate commodi suscipit harum aspernatur dolores, eum explicabo fuga facilis, amet excepturi ratione qui?"
+  },
+  {
+    id: '2',
+    countryName: 'France',
+    date: '8/08/2021',
+    flagIcon: 'fr',
+    activityIcon: '🇫🇷',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At earum alias a facere aperiam ipsum officia quaerat voluptas nisi. Molestiae ullam praesentium nisi. Tempora neque ea commodi numquam. Placeat ipsum sint esse! Alias mollitia similique sint adipisci aperiam blanditiis repellat ut quia, quisquam vitae perspiciatis nesciunt eius sed rerum harum cumque voluptatem dolore. Laudantium commodi autem voluptatem recusandae eligendi fugiat, officiis ipsa? Ut harum dignissimos impedit asperiores ex voluptatum optio vitae eligendi enim? Quam earum nisi voluptatem dolores illo mollitia beatae debitis. Error eligendi at quos cupiditate commodi suscipit harum aspernatur dolores, eum explicabo fuga facilis, amet excepturi ratione qui?"
+  },
+  {
+    id: '3',
+    countryName: 'Portugal',
+    date: '7/12/2021',
+    flagIcon: 'pt',
+    activityIcon: '🇵🇹',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At earum alias a facere aperiam ipsum officia quaerat voluptas nisi. Molestiae ullam praesentium nisi. Tempora neque ea commodi numquam. Placeat ipsum sint esse! Alias mollitia similique sint adipisci aperiam blanditiis repellat ut quia, quisquam vitae perspiciatis nesciunt eius sed rerum harum cumque voluptatem dolore. Laudantium commodi autem voluptatem recusandae eligendi fugiat, officiis ipsa? Ut harum dignissimos impedit asperiores ex voluptatum optio vitae eligendi enim? Quam earum nisi voluptatem dolores illo mollitia beatae debitis. Error eligendi at quos cupiditate commodi suscipit harum aspernatur dolores, eum explicabo fuga facilis, amet excepturi ratione qui?"
+  },
+  {
+    id: '4',
+    countryName: 'England',
+    date: '6/12/2021',
+    flagIcon: 'gb',
+    activityIcon: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At earum alias a facere aperiam ipsum officia quaerat voluptas nisi. Molestiae ullam praesentium nisi. Tempora neque ea commodi numquam. Placeat ipsum sint esse! Alias mollitia similique sint adipisci aperiam blanditiis repellat ut quia, quisquam vitae perspiciatis nesciunt eius sed rerum harum cumque voluptatem dolore. Laudantium commodi autem voluptatem recusandae eligendi fugiat, officiis ipsa? Ut harum dignissimos impedit asperiores ex voluptatum optio vitae eligendi enim? Quam earum nisi voluptatem dolores illo mollitia beatae debitis. Error eligendi at quos cupiditate commodi suscipit harum aspernatur dolores, eum explicabo fuga facilis, amet excepturi ratione qui?"
+  },
+  {
+    id: '5',
+    countryName: 'Morocco',
+    date: '5/12/2021',
+    flagIcon: 'ma',
+    activityIcon: '🇲🇦',
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At earum alias a facere aperiam ipsum officia quaerat voluptas nisi. Molestiae ullam praesentium nisi. Tempora neque ea commodi numquam. Placeat ipsum sint esse! Alias mollitia similique sint adipisci aperiam blanditiis repellat ut quia, quisquam vitae perspiciatis nesciunt eius sed rerum harum cumque voluptatem dolore. Laudantium commodi autem voluptatem recusandae eligendi fugiat, officiis ipsa? Ut harum dignissimos impedit asperiores ex voluptatum optio vitae eligendi enim? Quam earum nisi voluptatem dolores illo mollitia beatae debitis. Error eligendi at quos cupiditate commodi suscipit harum aspernatur dolores, eum explicabo fuga facilis, amet excepturi ratione qui?"
+  },
+]
--- src/components/shared/MyEntries.js
+++ src/components/shared/MyEntries.js
@@ -1,115 +1,17 @@
 import Image from 'next/image'
 import logo from '../../../public/logo.png'
+import Entry from './Entry'
+import { entriesData } from "../../../entryData"
 
 function MyEntries() {
   return (
     <div>
       <div className="h-screen ml-3 mt-4 pr-3">
-
-        <div className="mt-6">
-          <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-            <div className="w-full pl-4 py-2">
-              <p className="text-white">Kyoto, Japan</p>
-              <p className="text-white">12/17/2021</p>
-            </div>
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-          </div>
-
-          <div className="bg-white h-48 p-3 ">
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-          </div>
-        </div>
-
-        <div className="mt-6"> 
-          <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-            <div className="w-full pl-4 py-2">
-              <p className="text-white">Tokyo, Japan</p>
-              <p className="text-white">12/19/2021</p>
-            </div>
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-          </div>
-
-          <div className="bg-white h-48 p-3 ">
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-          </div>
-        </div>
-
-        <div className="mt-6"> 
-          <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-            <div className="w-full pl-4 py-2">
-              <p className="text-white">Tokyo, Japan</p>
-              <p className="text-white">12/19/2021</p>
-            </div>
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-          </div>
-
-          <div className="bg-white h-48 p-3 ">
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-          </div>
-        </div>
-
-        <div className="mt-6"> 
-          <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-            <div className="w-full pl-4 py-2">
-              <p className="text-white">Tokyo, Japan</p>
-              <p className="text-white">12/19/2021</p>
-            </div>
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-          </div>
-
-          <div className="bg-white h-48 p-3 ">
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-          </div>
-        </div>
-
-        <div className="mt-6"> 
-          <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-            <div className="w-full pl-4 py-2">
-              <p className="text-white">Tokyo, Japan</p>
-              <p className="text-white">12/19/2021</p>
-            </div>
-            <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-          </div>
-
-          <div className="bg-white h-48 p-3 ">
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-          </div>
-        </div>
-
+        {entriesData.map((entry) =>
+          <Entry key={entry.id} entry={entry} />
+        )}
       </div>
       
-      {/* <div className="ml-3 mt-4 pr-3">
-        <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
-
-          <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-          <div className="w-full pl-4 py-2">
-            <p className="text-white">Tokyo, Japan</p>
-            <p className="text-white">12/19/2021</p>
-          </div>
-          <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/>
-
-        </div>
-
-        <div className="bg-white h-48 p-3 ">
-          <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis temporibus dolore, suscipit possimus ut est, ducimus culpa aut nostrum eligendi velit molestias accusantium, quasi similique eius sequi quidem! Rem, nisi?</p>
-        </div>
-
-      </div> */}
 
     </div>
   )
