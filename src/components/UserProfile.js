444bd9102b65aaf32850067a6a30cbe137caef86
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-12 09:23:38 +0800

    Make logged in user name appear on header

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import logo from '../../public/logo.png'
 import MyEntries from './shared/MyEntries'
 import MyRolls from './shared/MyRolls'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import HeaderContext from '../contexts/HeaderContext'
 
 export const UserProfile = () => {
-  
   const [isMyEntriesPage, setIsMyEntriesPage] = useState(true);
   const [isMyRollsPage, setIsMyRollsPage] = useState(false);
+  const {loggedIn, loggedInUser, setLoggedIn, setLoggedInUser } = useContext(HeaderContext)
 
   const displayPage = (page) => {
     setIsMyEntriesPage(page === "myEntries" ? true : false)
@@ -16,34 +17,34 @@ export const UserProfile = () => {
 
   return (
     <>
-      {/* Profile header  */}
-      <div className="h-screen">
-        <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
-          <Image src={logo} height={'120'} width={'120'} className='bg-red-400 rounded-full mt-3'/>
-          {/* <p className="rounded-full w-32 h-32 bg-red-400 mt-3"></p> */}
-          <h1 className="text-white text-7xl">Leandre Kiu</h1>
-          <div className="flex justify-between w-56 h-1/4">
-            <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
-            <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
+    {loggedIn && 
+        <div className="h-screen">
+          <div style={{backgroundImage: `url("https://i.pinimg.com/736x/25/87/ec/2587ec34faa3b4bff7dbc7a2aa1692b1.jpg")` }} className="h-72 flex flex-col justify-center items-center">
+            <Image src={logo} height={'120'} width={'120'} className='bg-red-400 rounded-full mt-3'/>
+            {/* <p className="rounded-full w-32 h-32 bg-red-400 mt-3"></p> */}
+            <h1 className="text-white text-7xl">{loggedInUser.first_name}{' '}{loggedInUser.last_name}</h1>
+            <div className="flex justify-between w-56 h-1/4">
+              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Create Roll</a></button>
+              <button><a href="#" className="p-3 text-white bg-blue-500 rounded">Edit Profile</a></button>
+            </div>
+          </div>
+          
+          <div className="flex items-center h-full">
+            <p className="h-screen bg-gray-600 w-3/12 text-white mt-3 flex flex-col justify-center items-center justify-self-start self-start">bio/user stats</p>
+
+            <div className="h-full flex-grow w-9/12 //bg-red-900">
+              <button className={`${isMyEntriesPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3 ml-3`} onClick={() => displayPage("myEntries")}>My Entries</button>
+              <button className={`${isMyRollsPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3`} onClick={() => displayPage("myRolls")}>My Rolls</button>
+
+
+              {isMyEntriesPage && <MyEntries/>}
+              {isMyRollsPage && <MyRolls/>}
+
+            </div>
+
           </div>
         </div>
-        
-        {/* Sidebar & Main content */}
-        <div className="flex items-center h-full">
-          <p className="h-screen bg-gray-600 w-3/12 text-white mt-3 flex flex-col justify-center items-center justify-self-start self-start">bio/user stats</p>
-
-          <div className="h-full flex-grow w-9/12 //bg-red-900">
-            <button className={`${isMyEntriesPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3 ml-3`} onClick={() => displayPage("myEntries")}>My Entries</button>
-            <button className={`${isMyRollsPage ? "bg-purple-400" : "bg-gray-300"} px-8 py-3 mt-3`} onClick={() => displayPage("myRolls")}>My Rolls</button>
-
-
-            {isMyEntriesPage && <MyEntries/>}
-            {isMyRollsPage && <MyRolls/>}
-
-          </div>
-
-        </div>
-      </div>
+      }
     </>
   )
 }
