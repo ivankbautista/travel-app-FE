f99e17ebd9ced34a4dfd844e64bef2a6815db8a9
ivankbautista <ivankevinbautista@gmail.com>
2022-01-12 19:52:38 +0800

    Removes unnecessary top level div

--- src/components/shared/ExploreList.js
+++ src/components/shared/ExploreList.js
@@ -3,12 +3,10 @@ import ExploreEntry from "./ExploreEntry";
 
 function ExploreList() {
     return (
-        <div>
-            <div className="h-screen ml-3 mt-4 pr-3">
-                {exploreData.map((exploreEntry) =>
-                    <ExploreEntry key={exploreEntry.id} exploreEntry={exploreEntry} />
-                )}
-            </div>
+        <div className="h-screen ml-3 mt-4 pr-3">
+            {exploreData.map((exploreEntry) =>
+                <ExploreEntry key={exploreEntry.id} exploreEntry={exploreEntry} />
+            )}
         </div>
     )
 }
