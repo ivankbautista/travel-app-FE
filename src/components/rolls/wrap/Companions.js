aa4dba13c63cadc4a77d2c4dac594bae474bbfb5
Airene Maria <aireneapacible@gmail.com>
2022-01-14 19:53:18 +0800

    Change font to extrabold

--- src/components/rolls/wrap/Companions.js
+++ src/components/rolls/wrap/Companions.js
@@ -1,7 +1,7 @@
 import { RollBorder } from './RollBorder';
 export const Companions = (props) => {
     const array =  props.companions.filter(function(companion) {
-        return companion.category == "person";
+        return companion.category == "companion";
       });
     const content = array.map((companion) =>
     <div key={companion.id} className="rounded-full w-48 h-48 -rotate-6 bg-cover text-center m-2" style={{ backgroundImage:`url("${companion.image}")` }}><span className="bg-atlas-300 p-2 rounded-sm w-10 h-5 text-white text-s">{companion.title}</span></div>
@@ -12,7 +12,7 @@ export const Companions = (props) => {
             <RollBorder />
             <div className="grow flex items-center justify-center w-3/4">
                     <div className="flex flex-col text-xs">
-                        <div className="font-bold text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-atlas-300 to-atlas-100 text-center">
+                        <div className="font-extrabold text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-atlas-300 to-atlas-100 text-center">
                             You travelled with...
                         </div>
                         <div className="flex flex-wrap items-center justify-center flex-row h-56 w-[75vw] overflow-hidden p-5">
