bc8bb1a2036851b7967d5fd7966c2c44f4de75e0
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-14 22:41:25 +0800

    Make MyRoll component and make a Roll component inside the MyRoll component

--- src/components/shared/MyRolls.js
+++ src/components/shared/MyRolls.js
@@ -1,12 +1,43 @@
 import { useEffect, useState } from 'react' 
 import axios from 'axios'
 import { useRouter, Router } from 'next/router'
+import Roll from './Roll'
 
-function MyRolls() {
+function MyRolls({ currentUser }) {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(true);
+  const [rolls, setRolls] = useState()
+  const roll_id = router.query
+  // console.log(roll_id)
+  // console.log(RollPage.query.roll_id)        
+
+  const API = "http://localhost:3001"
+
+  useEffect(() => {
+    if(!router.isReady) return
+      axios({
+        method: 'GET',
+        url: `${API}/api/v1/rolls/user/${currentUser.id}`,
+      })
+        .then(function (response) {
+          console.log(response.data)
+          setRolls(response.data)
+          setLoading(false)
+        })
+    }, [router.isReady]);
+
+    if (isLoading) {
+      return <div className="App">Loading...</div>
+    }
+
+    // http://localhost:3001/api/v1/rolls/user/[user_id]
+
   return (
     <>
-      <div className="h-screen ml-3 mt-4 pr-3 overflow-auto">
-        <p className="text-white">My Rolls Page</p>
+      <div className="h-screen mt-4 pr-3">
+        {rolls.map((roll) => 
+          <Roll key={roll.id} roll={roll} />
+        )}
       </div>
     </>
   )
---
+++ src/components/shared/Roll.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+function Roll({ roll }) {
+  return (
+    <>
+    <div className="mt-6">
+      <div className="flex justify-between items-center py-3 px-3 bg-atlas-600">
+        {/* <Flag code={`${entry.flagIcon}`} className="w-10 h-10 rounded-full" /> */}
+        <div className="w-full pl-4 py-2">
+          <p className="text-white">{roll.title}</p>
+          <p className="text-white">{roll.start_date} - {roll.end_date}</p>
+          <Link href={`/rolls/${roll.id}`}>
+            <a className='flex justify-center items-center h-full px-6 text-atlas-100 hover:bg-atlas-500'>Show Roll</a>
+          </Link>
+        </div>
+        {/* <Image src={logo} height={'40'} width={'40'} layout="fixed" className='bg-red-400 rounded-full'/> */}
+      </div>
+    </div>
+  </>
+
+  )
+}
+
+export default Roll
