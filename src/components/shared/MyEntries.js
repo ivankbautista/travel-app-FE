e7b12a873c4a6cbe172b82c56d04596b826f1d15
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-01-14 22:43:12 +0800

    Minor change to Entry.js

--- src/components/shared/MyEntries.js
+++ src/components/shared/MyEntries.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-import logo from '../../../public/logo.png'
 import Entry from './Entry'
 import { entriesData } from "../../../entryData"
 
 function MyEntries() {
   return (
     <>
-      <div className="h-screen ml-3 mt-4 pr-3">
+      <div className="h-screen mt-4 pr-3">
         {entriesData.map((entry) =>
           <Entry key={entry.id} entry={entry} />
         )}
