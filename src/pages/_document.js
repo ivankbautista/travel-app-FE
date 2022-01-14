70b5e687ce18beea1ef08565fab13ddc6b6674fd
bananakiu <leandrenoelkiu@gmail.com>
2022-01-14 22:34:52 +0800

    add Title to every page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,8 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600" rel="stylesheet" />
+          <title>Atlas</title>
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <body class="bg-atlas-700">
           <Main />
