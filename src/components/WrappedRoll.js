7f88572702af681cc4b54b337f96e67fb98ebea0
Airene Maria <aireneapacible@gmail.com>
2022-01-14 13:59:31 +0800

    Fix naming and file structure

--- src/components/WrappedRoll.js
+++ src/components/WrappedRoll.js
@@ -6,7 +6,7 @@ import { Food } from './rolls/Food';
 import { Attractions } from './rolls/Attractions';
 import { Summary } from './rolls/Summary';
 
-export const Rolls = () => {
+export const WrappedRoll = () => {
     return (
         <div className="h-[92vh] bg-black flex flex-wrap overflow-hidden text-white">
             <div className="h-full w-full scroll-smooth overflow-y-scroll snap-mandatory snap-y">
