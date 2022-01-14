897aebdc212c96deed63d7c6dcb9b7dfe1f619e4
Airene Maria <aireneapacible@gmail.com>
2022-01-15 03:07:34 +0800

    Randomize country list

--- src/components/shared/CountryList.js
+++ src/components/shared/CountryList.js
@@ -1,9 +1,21 @@
 import CountryItem from './CountryItem'
 
 const CountryList = ({ countries }) => {
+    function shuffleArray(array) {
+        let i = array.length - 1;
+        for (; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+        return array;
+      }      
+
+    const country_random = shuffleArray(countries)
     return (
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            {countries.map((country) =>
+            {country_random.map((country) =>
                 <CountryItem key={country.name} country={country} />
             )}
         </div>
