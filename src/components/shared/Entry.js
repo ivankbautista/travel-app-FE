9e96577ece89746b7872c50cface7eee7c5cfb23
Airene Maria <aireneapacible@gmail.com>
2022-01-15 04:04:07 +0800

    Add entry title

--- src/components/shared/Entry.js
+++ src/components/shared/Entry.js
@@ -11,11 +11,10 @@ function Entry({ entry }) {
         <main className="flex w-full py-10 flex">
           <img src={entry.image} className="h-32 b rounded-2xl w-32 object-cover"></img>
 
-          <div className="ml-3 flex-col text-white">
+          <div className="ml-3 flex-col text-white font-body">
             <div className="pt-1">
-              <div className="flex mb-2">
-                <p>📌</p>
-                <span className="italic font-bold">{entry.city}</span>
+              <div className="flex mb-2 text-lg">
+                <p><span className="italic font-bold">{entry.title}</span> | 📌 <span className="italic font-bold">{entry.city}</span> </p>
               </div>
               <h2>{entry.category}</h2>
               <h2 className="font-semibold mt-1">{entry.date}</h2>
