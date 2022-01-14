84bcd4fdd7a19181ab44ec5de7c3436edce7ad1c
Airene Maria <aireneapacible@gmail.com>
2022-01-14 19:51:04 +0800

    Accomplish array mapping of accommodation component

--- src/components/rolls/wrap/Accommodation.js
+++ src/components/rolls/wrap/Accommodation.js
@@ -4,22 +4,24 @@ export const Accommodation = (props) => {
         return accommodation.category == "accommodation";
       });
     const content = array.map((accommodation) =>
-    <div key={accommodation.id} className="rounded-md w-40 h-40 m-1 -rotate-2 bg-cover" style={{ backgroundImage:`url("${accommodation.image}")` }}></div>
+    <div key={accommodation.id} className="rounded-md w-[25rem] h-[16rem] -rotate-3 p-2 pt-3 text-right m-2 bg-cover" style={{ backgroundImage:`url("${accommodation.image}")` }}></div>
   );
   return (
-    <div>
-        <div className="h-[92vh] bg-gradient-to-b from-[#51157d] to-[#381254] flex flex-wrap snap-center">
-            <RollBorder />
-            <div className="grow flex items-center justify-center">
-                <div className="flex flex-wrap w-[50vw] h-[70vh] items-center justify-center overflow-hidden">
-                {content}
+        <div>
+            <div className="h-[92vh] bg-gradient-to-b from-[#381254] to-atlas-600 flex flex-wrap snap-center">
+                <RollBorder />
+                <div className="grow flex items-center justify-center w-3/4">
+                    <div className="flex flex-col text-xs">
+                        <div className="font-bold text-5xl bg-clip-text text-transparent bg-gradient-to-tr from-atlas-300 to-atlas-100 text-center mt-5 mb-5 w-[75vw]">
+                            Enjoy your stay?
+                        </div>
+                        <div className="flex flex-wrap items-center justify-center flex-row h-[18rem] w-[75vw] overflow-hidden">
+                            {content}
+                        </div>
+                    </div>
                 </div>
-                <div className="font-extrabold text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-atlas-300 to-atlas-100 w-[30vw] text-center">
-                    Did you like the accommodation?
-                </div>
+                <RollBorder />
             </div>
-            <RollBorder />
         </div>
-    </div>
 )
 }
