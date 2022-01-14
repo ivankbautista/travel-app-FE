7d5429dbc0467cc73aba5d891c0c9d2b17c34ef1
Airene Maria <aireneapacible@gmail.com>
2022-01-15 00:58:29 +0800

    Style home page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,21 +6,22 @@ import CountryList from './shared/CountryList';
 export default function HomePage({ countries }) {
   return (
     <div>
-      <p className='mt-10 text-3xl text-center text-rose-500 font-bold underline font-display'>
-        Hi team! Here's our Homepage!
-      </p>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div>
-          <CountryList countries={countries} />
+        <div className="h-[92vh] bg-gradient-to-tr from-atlas-700 to-atlas-500 animate-gradient-xy flex flex-wrap snap-center">
+            <div className="grow flex items-center justify-center w-[80vw] flex-wrap">
+                <div className="rounded-md bg-atlas-100 flex flex-col font-extrabold text-6xl w-[50vw] text-left m-2 h-[73vh] overflow-scroll">
+                  <CountryList countries={countries} />
+                </div>
+                <div className="w-[29vw] m-2 h-[73vh] flex flex-col items-center justify-center">
+                    <div className="w-[29vw] text-center flex flex-col items-center justify-center">
+                        <div className="rounded-md w-[29vw] h-[35vw] bg-cover bg-[url('https://i.pinimg.com/736x/73/01/8b/73018baf7cd5c62334801095c05e06b6.jpg')]"></div>
+                        <div className="font-extrabold text-4xl bg-clip-text text-transparent bg-gradient-to-tr from-atlas-300 to-atlas-100 text-center">
+                            Welcome to ATLAS.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <br />
-        <button className="bg-rose-400 hover:bg-green-500 text-white hover:text-white font-bold py-2 px-4 rounded">
-          This is a button.
-        </button>
-
-      </main>
-    </div>
-  )
+    </div>  )
 }
 
 export const getStaticProps = async () => {
