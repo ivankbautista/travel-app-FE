2cab8bfa842544be10650bda9e63efd059c1022d
ivankbautista <ivankevinbautista@gmail.com>
2022-01-15 03:06:22 +0800

    Finalize axios calls, and map through entries

--- src/components/shared/ExploreList.js
+++ src/components/shared/ExploreList.js
@@ -1,13 +1,48 @@
-import { exploreData } from "../../../exploreData";
-import ExploreEntry from "./ExploreEntry";
+// import { exploreData } from "../../../exploreData";
+import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios';
+import { useRouter, Router } from 'next/router'
+import { useState } from 'react';
+import { useContext } from 'react';
+import HeaderContext from '../../contexts/HeaderContext';
+import ExploreEntry from './ExploreEntry';
 
-function ExploreList() {
+export const ExploreList = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [entries, setEntries] = useState();
+
+    const router = useRouter();
+    const country_name = router.query.name
+    const API = "http://localhost:3001"
+    const { loggedInUser } = useContext(HeaderContext)
+    console.log(country_name)
+
+    useEffect(() => {
+        if (!router.isReady) return
+        axios({
+            method: 'GET',
+            url: `${API}/api/v1/entries/country/${country_name}`,
+        })
+            .then(function (response) {
+                console.log(response.data)
+                setEntries(response.data);
+                setLoading(false);
+            });
+    }, [router.isReady]);
+
+    if (isLoading) {
+        return <div className="App">Loading...</div>;
+    }
+
     return (
-        <div className="h-screen ml-3 mt-4 pr-3">
-            {exploreData.map((exploreEntry) =>
-                <ExploreEntry key={exploreEntry.id} exploreEntry={exploreEntry} />
-            )}
-        </div>
+        < div className="h-screen pr-3" >
+            {
+                entries.map((entry) =>
+                    <ExploreEntry key={entry.id} exploreEntry={entry} />
+                )
+            }
+        </div >
     )
 }
 
